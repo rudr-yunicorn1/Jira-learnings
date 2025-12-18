205e8878e55bb9c1127c7203cdb78c3733f01cc4
rudresh dixit <rudresh.dixit@yunicor.in>
2025-12-18 12:33:16 +0530

    MT-109 What is Nodemailer?

--- 5.5_node_nodmailer/1_nodemailer.js
+++ 5.5_node_nodmailer/1_nodemailer.js
@@ -9,13 +9,31 @@ const app=express();
 let port=5000
 
 // importing the sendmail func form the controllers
-const sendMail=require('./controller/1.1_sendMail')
 // now iam calling with the api to use mail functionality 
-app.get('/sendmail',sendMail)
+app.get('/sendmail',async (req,res)=>{ // this is the function which is called from the app. js file to send the mail 
+    // need to create a transport layer 
+     const transporter =await nodemailer.createTransport({// this the server we have created and also using auth to verify the sender
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+        user: "delphine.hartmann@ethereal.email",  // senders email id 
+        pass: "djf443SpjzscH6yuKF", // senders paswoed 
+    },
+    })
+    const info = await transporter.sendMail({
+        from: '"Rudresh Dixit" <delphine.hartmann@ethereal.email>', // senders address
+        to: "r.techrt123@gmail.com", // recivers address
+        subject: "Hello ✔",                             // subject  of the mail 
+        text: "Hello world?", // plain‑text body
+        html: "<b>Hello world?</b>", // HTML body
 
-app.get('/',(req,res)=>{
-    res.send("home page")
-})
+    })
+    console.log("Message sent:", info.messageId);
+    res.json(info)
+  });
+
+
 
 const start=async()=>{
     try{
